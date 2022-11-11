df6f94845334c27159c0d780a6709c9999ee515d
Ewan724 <96613098+Ewan724@users.noreply.github.com>
2022-11-11 16:31:43 +0800

    Add files via upload

---
+++ Q2/solution.py
@@ -0,0 +1,15 @@
+N = input()
+allValid = True
+errorCodes = []
+for i in range(N):
+    record = input().split(' ')
+    if record[1] == 'false':
+        allValid = False
+        errorCodes.append[record[2]]
+
+if allValid == True:
+    print("Yes")
+else:
+    print("No")
+    print(f'{errorCodes}'.replace('[', '').replace(']', '').replace("'", '').replace(",", ''))
+
